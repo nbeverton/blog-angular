722b5b5efaac8d2895b7eff312fd4f25cc3a0884
nbeverton <104376567+nbeverton@users.noreply.github.com>
2024-04-11 15:36:21 -0300

    big-card component
    The big-card component was created.

--- blog-angular/src/app/app.component.ts
+++ blog-angular/src/app/app.component.ts
@@ -4,13 +4,14 @@ import { BigCardComponent } from "./components/big-card/big-card.component";
 import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
 import { MenuTitleComponent} from "./components/menu-title/menu-title.component"
 import { SmallCardComponent } from "./components/small-card/small-card.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, BigCardComponent, MenuBarComponent, MenuTitleComponent, SmallCardComponent]
+    imports: [RouterOutlet, BigCardComponent, MenuBarComponent, MenuTitleComponent, SmallCardComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'blog-angular';
